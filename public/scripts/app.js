2ebf334300fc84642f1291d67a7bcd0992af81ed
NurlybekSultamuratov <nsultamu@my.centennialcollege.ca>
2020-10-09 22:52:05 -0700

    added comments

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,11 +1,15 @@
+//app.js
+
+// Name: Nurlybek Sultamuratov
+// ID:   301103746
+// Date: 2020-10-09 
+
 (function()
 {
     function Start()
     {
         console.log("App Started");
     }
-
     window.addEventListener("load",Start);
-
 })();
 
